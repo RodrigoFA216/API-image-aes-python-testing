f89c837fe7f255e7f57cfaefcf0d77d2c2c7ce5b
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-03-26 19:29:00 -0600

    creación de carpeta con imagen

--- app/routes/router.py
+++ app/routes/router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, UploadFile, File
 import shutil
 from typing import List
 from fastapi.responses import JSONResponse, FileResponse
+from os import getcwd
+import os
 
 from app.schemas.item_scheme import ItemScheme
 
@@ -51,3 +53,22 @@ async def reciveImage(file: UploadFile = File(...)):
             return JSONResponse(content={"Error": "La extención del archivo no es válida"}, status_code=200)
     except:
         return JSONResponse(content={"Error": "Algo falló con el archivo"}, status_code=205)
+
+
+@router.post('/API/Encrypt/Image/V3', tags=['Post', 'Recive Imagen', 'encrypt'])
+async def reciveImage(file: UploadFile = File(...)):
+    if file.filename[-4:] in imgFormats:
+        # Uno la ruta de imgFolder con el nombre del archivo menos la extensión
+        file_folder = os.path.join(imgFolder, file.filename[:-4])
+        # Creo la ruta final del archivo
+        os.makedirs(file_folder, exist_ok=True)
+        # Guardo el archivo dentro de la carpeta
+        file_path = os.path.join(file_folder, file.filename)
+        with open(file_path, 'wb') as F:
+            content = await file.read()
+            F.write(content)
+            F.close()
+        path = f'{imgFolder}{file.filename}'
+        return FileResponse(file_path)
+    else:
+        return JSONResponse(content={"Error": "La extención del archivo no es válida"}, status_code=200)
