87069f5a5d75eea17165eea6b5ac72dc97ea1850
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-04-19 12:38:12 -0600

    change in requirements

--- app/functions/AES_decrypt.py
+++ app/functions/AES_decrypt.py
@@ -25,29 +25,29 @@ from Crypto.Util.Padding import unpad
 # cv2.imwrite('app/temp/img/Atmosfera/Atmosfera-uncif.jpg', img_descifrada)
 
 
-def decrypt_image(key, vector, path, name):
-    img_cifrada = cv2.imread(path)
-    cipher = AES.new(key, AES.MODE_OFB, iv)
-    img_descifrada = cipher.decrypt(img_cifrada)
-    img_np = np.frombuffer(img_descifrada, np.uint8)
-    img_np = img_np.reshape((img_cifrada.shape[0], img_cifrada.shape[1], -1))
-    cv2.imshow("Imagen descifrada", img_np)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    # with open(path, 'rb') as F:
-    #     img_cifrada = F.read()
-    # aes = AES.new(key, AES.MODE_OFB, vector)
-    # img_descifrada = unpad(aes.decrypt(img_cifrada), AES.block_size)
-    # cv2.imshow('Imagen descifrada', img_descifrada.astype('uint8'))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # img_descifrada = cv2.imdecode(
-    #     numpy.frombuffer(img_descifrada, numpy.uint8),
-    #     cv2.IMREAD_GRAYSCALE
-    # )
-    # cv2.imwrite(path[:-len(name)]+name[:-4]+'-decif'+name[-4:], img_descifrada)
-    # string = f'{path[:-len(name)]}{name[:-4]}-decif{name[-4:]}'
-    # return string
+# def decrypt_image(key, vector, path, name):
+#     img_cifrada = cv2.imread(path)
+#     cipher = AES.new(key, AES.MODE_OFB, iv)
+#     img_descifrada = cipher.decrypt(img_cifrada)
+#     img_np = np.frombuffer(img_descifrada, np.uint8)
+#     img_np = img_np.reshape((img_cifrada.shape[0], img_cifrada.shape[1], -1))
+#     cv2.imshow("Imagen descifrada", img_np)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+# with open(path, 'rb') as F:
+#     img_cifrada = F.read()
+# aes = AES.new(key, AES.MODE_OFB, vector)
+# img_descifrada = unpad(aes.decrypt(img_cifrada), AES.block_size)
+# cv2.imshow('Imagen descifrada', img_descifrada.astype('uint8'))
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# img_descifrada = cv2.imdecode(
+#     numpy.frombuffer(img_descifrada, numpy.uint8),
+#     cv2.IMREAD_GRAYSCALE
+# )
+# cv2.imwrite(path[:-len(name)]+name[:-4]+'-decif'+name[-4:], img_descifrada)
+# string = f'{path[:-len(name)]}{name[:-4]}-decif{name[-4:]}'
+# return string
 
 
 async def decipher_image(key, vector, path, name):
